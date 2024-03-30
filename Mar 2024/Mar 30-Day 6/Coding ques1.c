9b9214ca568d45df756dec0a74beeb97b81d3378
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-03-30 13:55:44 +0530

    Create Coding ques1.c

---
+++ Mar 2024/Mar 30-Day 6/Coding ques1.c
@@ -0,0 +1,7 @@
+Get an integer from the user and print lucky number if the sum of odd numbers in a number is even else print not lucky number.
+Sample Input 1:                            Sample Input 2:
+351                                        5638
+Output:                                    Output:
+Not lucky number                           Lucky number
+Explanation:                               Explanation:
+3+5+1=9.9 is not an even number            5+3=8. 8 is an even number
