2f798117559e4740cc8912794bbae64d63d15a34
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-03-31 21:36:23 +0530

    Update coding ques1.c

--- Mar 2024/Mar 29-day5/coding ques1.c
+++ Mar 2024/Mar 29-day5/coding ques1.c
@@ -1,1 +1,21 @@
 /*Pattern printing right half pyramid*/
+#include <stdio.h>
+int main()
+{
+   for(int i=1;i<=5;i++)
+   {
+       for(int j=1;j<=i;j++)
+       {
+           printf("* ");
+       }
+       printf("\n");
+   }
+}
+/*
+Output:
+*
+* *
+* * * 
+* * * *
+* * * * *
+*/
