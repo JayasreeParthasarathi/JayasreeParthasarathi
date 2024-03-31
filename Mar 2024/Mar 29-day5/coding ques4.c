4fb7c9a2a09bc0a68e7150efeaed7d3fe25ce93f
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-03-31 22:20:34 +0530

    Update coding ques4.c

--- Mar 2024/Mar 29-day5/coding ques4.c
+++ Mar 2024/Mar 29-day5/coding ques4.c
@@ -1,1 +1,31 @@
 /*Inverted full pyramid*/
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter value of n:");
+    scanf("%d",&n);
+    for(i=n;i>=1;--i)
+   {
+       for(j=1;j<=n-i;++j)
+       {
+           printf(" ");
+       }
+       for(j=1;j<=i;++j)
+       {
+           printf("* ");
+       }
+       printf("\n");
+   }
+   return 0;
+}
+/*
+Input:
+Enter value of n:5
+Output:
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
