6d96f3230dc294db99fc447ad316ecce4b730304
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-03 21:14:24 +0530

    Update coding ques1.c

--- Apr 2024/Apr 3-day 10/coding ques1.c
+++ Apr 2024/Apr 3-day 10/coding ques1.c
@@ -4,3 +4,25 @@
      output:
           5
 */
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int x1,y1,x2,y2;
+    printf("(x1,y1)=");
+    scanf("(%d,%d)",&x1,&y1);
+    printf("(x2,y2)=");
+    scanf("\n(%d,%d)",&x2,&y2);
+    int n,m,r;
+    n=(x2-x1)*(x2-x1);
+    m=(y2-y1)*(y2-y1);
+    r=sqrt(n+m);
+    printf("%d",r);
+    return 0;
+}
+/*Input:
+(x1,y1)=(5,5)
+(x2,y2)=(8,9)
+Output:
+5
+*/
