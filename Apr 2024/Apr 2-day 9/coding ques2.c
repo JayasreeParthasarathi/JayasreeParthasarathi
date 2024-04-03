f33ca46bd2068be08ef66705ce5ee059a63f4dae
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-03 20:57:27 +0530

    Create coding ques2.c

---
+++ Apr 2024/Apr 2-day 9/coding ques2.c
@@ -0,0 +1,6 @@
+/*2.2. Divide two integers without using division, multiplication and modulus operators
+Example input:
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = 3.33333.. which is truncated to 3.
+*/
