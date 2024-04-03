cf87bb117ffb5122021143007d0a927a8c4c2653
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-03 22:17:05 +0530

    Update coding ques2.c

--- Apr 2024/Apr 3-day 10/coding ques2.c
+++ Apr 2024/Apr 3-day 10/coding ques2.c
@@ -4,3 +4,28 @@
     output:
         h t e
 */
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    scanf("%[^\n]",s);
+    int c[256]={0},i;
+    for(i=0;i<strlen(s);i++)
+    {
+        c[s[i]]++;
+    }
+    for(i=0;i<strlen(s);i++)
+    {
+        if(c[s[i]]==1)
+        {
+            printf("%c ",s[i]);
+        }
+    }
+    return 0;
+}
+/*Input:
+  Jayasree
+  Output:
+  J y s r
+*/
