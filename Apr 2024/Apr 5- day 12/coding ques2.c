ccb8a094910f1b975e1ada5feabbba0d865386ef
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-05 23:33:30 +0530

    Update coding ques2.c

--- Apr 2024/Apr 5- day 12/coding ques2.c
+++ Apr 2024/Apr 5- day 12/coding ques2.c
@@ -1,1 +1,27 @@
 /* Write a program to Check Whether a Number is Positive, Negative, or Zero.*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n==0)
+    {
+        printf("The number is zero");
+    }
+    else if(n>0)
+    {
+        printf("Positive number");
+    }
+    else
+    {
+        printf("Negative number");
+    }
+    return 0;
+}
+/*
+Input:
+Enter the number:23
+Output:
+Positive number
+*/
