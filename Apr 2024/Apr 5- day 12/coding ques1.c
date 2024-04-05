87330134e3c9ef74106ad3f3e9adecc1184dfd9a
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-05 23:29:15 +0530

    Update coding ques1.c

--- Apr 2024/Apr 5- day 12/coding ques1.c
+++ Apr 2024/Apr 5- day 12/coding ques1.c
@@ -1,1 +1,18 @@
 /*Write a program to Calculate Fahrenheit to Celsius.*/
+#include<stdio.h>
+int main()
+{
+    int f;
+    float r;
+    printf("Enter the fahrenheit value:");
+    scanf("%d",&f);
+    r=((f-32)*5)/9;
+    printf("%.3f",r);
+    return 0;
+}
+/*
+Input:
+Enter the fahrenheit value:78
+Output:
+25.000
+*/
