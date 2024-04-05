781e886c96001b661c55a0b81ff32c111598bab9
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-05 23:10:31 +0530

    Update coding ques2.c

--- Apr 2024/Apr 2-day 9/coding ques2.c
+++ Apr 2024/Apr 2-day 9/coding ques2.c
@@ -4,5 +4,23 @@ Input: dividend = 10, divisor = 3
 Output: 3
 Explanation: 10/3 = 3.33333.. which is truncated to 3.
 */
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,d,r;
+    printf("Dividend:");
+    scanf("%d",&n);
+    printf("Divisor:");
+    scanf("%d",&d);
+    r=floor(n/d);
+    printf("%d",r);
+    return 0;
+}
 /*
+Input:
+Dividend:10
+Divisor:3
+Output:
+3
 */
