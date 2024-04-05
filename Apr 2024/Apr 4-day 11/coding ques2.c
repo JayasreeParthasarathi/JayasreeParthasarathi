69e80a932bf64e50a5cb3007d35092423b135bbb
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-05 22:40:07 +0530

    Update coding ques2.c

--- Apr 2024/Apr 4-day 11/coding ques2.c
+++ Apr 2024/Apr 4-day 11/coding ques2.c
@@ -1,1 +1,23 @@
 /*2.)Write a C Program to check if two numbers are equal without using the bitwise operator*/
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    printf("Enter two values:");
+    scanf("%d %d",&n,&m);
+    if(n-m==0)
+    {
+        printf("The numbers are equal");
+    }
+    else
+    {
+        printf("The numbers are not equal");
+    }
+    return 0;
+}
+/*
+Input:
+Enter two values:30 30
+Output:
+The numbers are equal
+*/
