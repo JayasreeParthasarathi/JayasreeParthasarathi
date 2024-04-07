80e7a7305e93b0a40b60b5ea7c5eb2e0103e53d6
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-07 23:14:02 +0530

    Update coding ques1.c

--- Apr 2024/Apr 6-Day 13/coding ques1.c
+++ Apr 2024/Apr 6-Day 13/coding ques1.c
@@ -1,1 +1,19 @@
 /*Find the Length of a String*/
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    printf("Enter a string:");
+    scanf("%[^\n]",s);
+    int l=strlen(s);
+    printf("%d",l);
+    return 0;
+}
+/*
+Input:
+Enter a string:I am jayasree
+Output:
+13
+*/
