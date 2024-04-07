bcef2efa885d29036f20fc1027aed21d2d499ab3
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-07 23:25:03 +0530

    Update coding ques2.c

--- Apr 2024/Apr 6-Day 13/coding ques2.c
+++ Apr 2024/Apr 6-Day 13/coding ques2.c
@@ -1,1 +1,31 @@
 /*Concatenate Two Strings*/
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[1000],s2[1000];
+    printf("Enter two strings:");
+    scanf("%[^\n]\n%[^\n]",s1,s2);
+    
+    int i=0,j=0;
+    while(s1[i]!='\0')
+    {
+        i++;
+    }
+    while(s2[j]!='\0')
+    {
+        s1[i]=s2[j];
+        i++;
+        j++;
+    }
+    printf("%s",s1);
+    return 0;
+}
+/*
+Input:
+Enter two strings:Hello!
+I am jayasree
+Output:
+Hello!I am jayasree
+*/
