3c098e12b86a891c1f0a813acfbbf924f79e475c
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-08 18:59:34 +0530

    Create coding ques1.c

---
+++ Apr 2024/Apr 8-Day 15/coding ques1.c
@@ -0,0 +1,14 @@
+/*1.Given an array form a triangle such that the last row of the triangle contains all the elements of the array and the row above it will contain the sum of two elements below it.
+
+Sample Input
+
+arr[] = {4, 7, 3, 6, 7};
+
+Sample Output
+
+81
+40 41
+21 19 22
+11 10 9 13
+4 7 3 6 7
+*/
