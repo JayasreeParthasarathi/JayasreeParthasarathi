75b22f0f0d773d8c737c4b996168e4c5dd40627a
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-09 23:15:00 +0530

    Update coding ques2.c

--- Apr 2024/Apr 8-Day 15/coding ques2.c
+++ Apr 2024/Apr 8-Day 15/coding ques2.c
@@ -7,3 +7,37 @@ Move#Hash#to#Front
 Output:
 
 ###MoveHashtoFront*/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    printf("Enter the string:");
+    scanf("%[^\n]",s);
+    int i,c=0;
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='#')
+        {
+            c++;
+        }
+    }
+    for(i=0;i<c;i++)
+    {
+        printf("#");
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]!='#')
+        {
+            printf("%c",s[i]);
+        }
+    }
+    return 0;
+}
+/*
+Input:
+Enter the string:#Jaya#sree#
+Output:
+###Jayasree
+*/
