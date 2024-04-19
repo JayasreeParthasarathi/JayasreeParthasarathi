93129401d61867b395a58fcc147c4eb6badac387
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-20 00:42:32 +0530

    Create Coding ques1.c

---
+++ Apr 2024/Apr 18-day 24/Coding ques1.c
@@ -0,0 +1,15 @@
+//1.Write a program to find GCD of two numbers.
+
+#include<stdio.h>
+int gcd(int a, int b)
+{
+	if (a==0)
+		return b;
+	return gcd(b%a,a);
+}
+int main()
+{
+    int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",gcd(a,b));
+}
