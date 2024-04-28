42b162388c7441b2ba12997261be54a6394260da
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-28 22:53:46 +0530

    Create coding ques2.py

---
+++ Apr 2024/Apr 27-day 29/coding ques2.py
@@ -0,0 +1,9 @@
+'''Write a program to find Maximum and Minimum in a Set'''
+
+
+input_str=input("enter elements separated by commas: ")
+a=set(input_str.split(','))
+print("Set A :",a)
+print("\nmaxi of a :",max(a))
+print("mini of a :",min(a))
+
