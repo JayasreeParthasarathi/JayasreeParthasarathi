0d4669eda43187f60bfef1f04efe872ade94f4c0
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-04-28 22:52:12 +0530

    Create coding ques1.c

---
+++ Apr 2024/Apr 27-day 29/coding ques1.c
@@ -0,0 +1,19 @@
+/*Write a Program to Find fibonacci series upto N*/
+
+#include<stdio.h>
+int main()
+{
+  int i,n;
+  int t1=0,t2=1;
+  int next=t1+t2;
+  printf("number of terms: ");
+  scanf("%d",&n);
+  printf("fibonacci series: %d, %d, ", t1, t2);
+  for(i=3;i<=n;i++)
+  {
+    printf("%d, ", next);
+    t1=t2;
+    t2=next;
+    next=t1+t2;
+  }
+}
