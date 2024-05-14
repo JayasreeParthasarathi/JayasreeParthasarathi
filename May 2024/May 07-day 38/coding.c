9435a2cda3f95f0b79bb2d727a4dbc5fe747ce10
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-05-14 20:59:09 +0530

    Update coding.c

--- May 2024/May 07-day 38/coding.c
+++ May 2024/May 07-day 38/coding.c
@@ -1,1 +1,21 @@
 /*Program to print quotient and remainder*/
+
+#include<stdio.h>
+int main()
+{
+    int n,d;
+    printf("Enter numerator and denominator:");
+    scanf("%d %d",&n,&d);
+    int q=n/d;
+    int r=n%d;
+    printf("Quotient=%d",q);
+    printf("\nRemainder=%d",r);
+    return 0;
+}
+/*
+Input:
+Enter numerator and denominator:9 2
+Output:
+Quotient=4
+Remiander=1
+*/
