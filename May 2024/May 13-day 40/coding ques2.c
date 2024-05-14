a35aa6a6190c75ed5c21b832c704221a08c5eab3
JayasreeParthasarathi <157371972+JayasreeParthasarathi@users.noreply.github.com>
2024-05-14 19:25:21 +0530

    Create coding ques2.c

---
+++ May 2024/May 13-day 40/coding ques2.c
@@ -0,0 +1,4 @@
+/*2.Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+Input: nums = [-2,-1,-1,1,2,3]
+Output: 3
+Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.*/
